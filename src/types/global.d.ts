b3026aee8aaf9da1dde79948f403b97321b677c1
David  Delaney <daviddelaney@Davids-MacBook-Air.local>
2025-03-07 16:08:48 -0800

    Fixed ESLint and TypeScript issues for deployment

---
+++ src/types/global.d.ts
@@ -0,0 +1,13 @@
+declare module 'heic2any' {
+  interface Options {
+    blob: Blob;
+    toType?: string;
+    quality?: number;
+  }
+  function heic2any(options: Options): Promise<Blob | Blob[]>;
+  export default heic2any;
+}
+
+declare module 'pdfjs-dist/webpack' {
+  export * from 'pdfjs-dist';
+} 
